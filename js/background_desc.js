01da402ba36253b737853ea8c3545dd6d31fa7fe
jlainez <jorgeld1986@gmail.com>
2018-03-06 14:35:40 +0100

    Refactorización nueva versión

---
+++ js/background_desc.js
@@ -0,0 +1,3 @@
+document.getElementById('barberia_desc').style.backgroundImage = "url(css/images/cliente2.jpg)";
+document.getElementById('tienda_desc').style.backgroundImage = "url(css/images/acua41.jpg)";
+document.getElementById('eventos_desc').style.backgroundImage = "url(css/images/bombilla.jpg)";
