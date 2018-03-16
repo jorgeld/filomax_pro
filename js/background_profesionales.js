4ce6b9c60e2eb2236825284c6a584be5aadb2b47
jlainez <jorgeld1986@gmail.com>
2018-03-16 12:13:59 +0100

    Añadido barbero

--- js/background_profesionales.js
+++ js/background_profesionales.js
@@ -1,3 +1,4 @@
 document.getElementById('barbero1').style.backgroundImage = "url(./images/barberos/barbero1.jpg)";
 document.getElementById('barbero2').style.backgroundImage = "url(./images/barberos/barbero2.jpg)";
 document.getElementById('barbero3').style.backgroundImage = "url(./images/barberos/barbero3.jpg)";
+document.getElementById('barbero4').style.backgroundImage = "url(./images/barberos/barbero4.jpg)";
